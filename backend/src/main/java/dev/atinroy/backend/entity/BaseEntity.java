4f3a3ba2f30eb179b8d8eee47319c6647aacdaa9
Atin Roy <me@atinroy.dev>
2026-01-29 22:09:46 +0530

    feat: add base entity metadata

--- backend/src/main/java/dev/atinroy/backend/entity/BaseEntity.java
+++ backend/src/main/java/dev/atinroy/backend/entity/BaseEntity.java
@@ -1,4 +1,35 @@
 package dev.atinroy.backend.entity;
 
-public class BaseEntity {
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updatedAt;
 }
